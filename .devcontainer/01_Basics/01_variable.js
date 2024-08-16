11907e623026f63c334efdcf912a8889a9e46bb2
Muhammad Arham <m.arhamzahid18@gmail.com>
2024-08-16 13:52:32 +0000

    javascript

---
+++ .devcontainer/01_Basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 1333234
+let accountEmail = "arham@google.com"
+var accountPassword = "54321"
+accountCity = "Karachi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
